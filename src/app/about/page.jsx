df70c7268fda17de4464d6fba437c445b2a1230d
MMunim <mmunim9.9.01@gmail.com>
2025-08-01 14:28:34 +0600

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,28 @@
+"use client"
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+const AboutPage = () => {
+
+    const router = useRouter();
+    const isloggedIn = true;
+    const handleNavigation = () => {
+        if(isloggedIn){
+            router.push("/about/address")
+        }
+        else{
+            router.push("/")
+        }
+    };
+    return (
+        <div>
+            <p className='font-bold text-3xl'>about page</p>
+            <Link href="/about/address">Address Page</Link>
+
+            <button type="button" onClick={handleNavigation}>Address page</button>
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/app/services/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServicePage = () => {
+    return (
+        <div>
+            <p className='font-bold text-3xl'>Services page</p>
+        </div>
+    );
+};
+
+export default ServicePage;
